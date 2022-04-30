ba9ee2badee27f057d572892b20b16baeb04b2d2
jonascolas <jonas@fusemachines.com>
2022-04-30 12:46:20 -0300

    initial state, setState, constructor and super in Class Component

---
+++ src/components/User.js
@@ -0,0 +1,19 @@
+import classes from './User.module.css';
+import { Component } from 'react';
+
+class User extends Component {
+  // constructor(props) {}
+
+  render() {
+    return (
+      <li className={classes.User}>{this.props.name}</li>
+    )
+  }
+}
+
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
+
+export default User;
